c7ee98f7a18f51472fabbf629e1bc98b7290c3fe
Alex Ignatenko <ignatenko2207@gmail.com>
2018-04-22 14:32:12 +0300

    Change entities

--- TestTimeTable/src/main/java/org/itstep/model/Student.java
+++ TestTimeTable/src/main/java/org/itstep/model/Student.java
@@ -1,12 +1,8 @@
 package org.itstep.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,25 +11,8 @@ import lombok.Setter;
 @Getter
 @Entity
 @Table( name = "STUDENTS")
-public class Student {
+public class Student extends User{
 
-	@Id
-	@Column(name = "LOGIN")
-	@JsonProperty
-	private String login;
-	
-	@Column(name = "PASSWORD")
-	@JsonProperty
-	private String password;
-	
-	@Column(name = "FIRS_TNAME")
-	@JsonProperty
-	private String firstName;
-	
-	@Column(name = "SECOND_NAME")
-	@JsonProperty
-	private String secondName;
-	
 	@ManyToOne(targetEntity = Group.class)
 	private Group group;
 }
--- TestTimeTable/src/main/java/org/itstep/model/Teacher.java
+++ TestTimeTable/src/main/java/org/itstep/model/Teacher.java
@@ -1,12 +1,8 @@
 package org.itstep.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,24 +11,7 @@ import lombok.Setter;
 @Getter
 @Entity
 @Table( name = "TEACHERS")
-public class Teacher {
-	
-	@Id
-	@Column(name = "LOGIN")
-	@JsonProperty
-	private String login;
-	
-	@Column(name = "PASSWORD")
-	@JsonProperty
-	private String password;
-
-	@Column(name = "FIRST_NAME")
-	@JsonProperty
-	private String firstName;
-	
-	@Column(name = "SECONDNAME")
-	@JsonProperty
-	private String secondName;
+public class Teacher extends User{
 	
 	@ManyToOne(targetEntity = Subject.class)
 	private Subject subject;
