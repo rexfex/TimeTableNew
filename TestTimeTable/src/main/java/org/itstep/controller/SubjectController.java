96e07c175eef245c15db636577199ede5f7e9087
Alex Ignatenko <ignatenko2207@gmail.com>
2018-04-22 15:14:23 +0300

    Fix controllers

--- TestTimeTable/src/main/java/org/itstep/controller/SubjectController.java
+++ TestTimeTable/src/main/java/org/itstep/controller/SubjectController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -42,7 +43,7 @@ public class SubjectController {
 	}
 
 	@GetMapping( produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
-	ResponseEntity<Subject> getOne(@RequestBody String name) {
+	ResponseEntity<Subject> getOne(@RequestHeader String name) {
 		Subject subject = subjectService.get(name);
 		if (subject != null) {
 			return new ResponseEntity<Subject>(subject, HttpStatus.OK);
